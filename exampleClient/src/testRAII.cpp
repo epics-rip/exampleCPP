6323525e41e3fabfdb5a11f408106248d82d0ad5
mrkraimer <mrkraimer@comcast.net>
2016-05-20 06:40:40 -0400

    working on RAII

--- exampleClient/src/testRAII.cpp
+++ exampleClient/src/testRAII.cpp
@@ -21,6 +21,7 @@ using namespace epics::pvaClient;
 
 static void example(PvaClientPtr const &pva,string const & channelName,string const & providerName )
 {
+    cout << "_____example starting_______\n";
     PvaClientChannelPtr pvaChannel = pva->createChannel(channelName,providerName);
     pvaChannel->issueConnect();
     Status status = pvaChannel->waitConnect(2.0);
@@ -29,6 +30,9 @@ static void example(PvaClientPtr const &pva,string const & channelName,string co
     pvaGet->issueConnect();
     status = pvaGet->waitConnect();
     if(!status.isOK()) {cout << " createGet failed\n"; return;}
+    PvaClientGetDataPtr pvaData = pvaGet->getData();
+    double value = pvaData->getDouble();
+    cout << "as double " << value << endl;
     cout << "__example__ returning\n";
 }
 
@@ -40,15 +44,16 @@ int main(int argc,char *argv[])
         PvaClientPtr pva= PvaClient::get("pva");
 PvaClient::setDebug(true);
         example(pva,"PVRdouble","pva");
-epicsThreadSleep(3.0);
-cout << "after epicsThreadSleep\n";
+cout << "after example\n";
+//epicsThreadSleep(3.0);
+//cout << "after epicsThreadSleep\n";
         cout << "_____testRAII done_______\n";
     } catch (std::runtime_error e) {
         cerr << "exception " << e.what() << endl;
         return 1;
     }
     cout << "after pvaClient looses context\n";
-epicsThreadSleep(3.0);
-cout << "after epicsThreadSleep\n";
+//epicsThreadSleep(3.0);
+//cout << "after epicsThreadSleep\n";
     return 0;
 }
