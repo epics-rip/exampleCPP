ee91e12b92b4a732d4a79d8007ea3ceec346dfa9
mrkraimer <mrkraimer@comcast.net>
2016-07-25 12:27:30 -0400

    exampleHelloRPCRecord must return clone; simplify exampleHelloRecord

--- database/src/pv/exampleHelloRPCRecord.h
+++ database/src/pv/exampleHelloRPCRecord.h
@@ -33,52 +33,50 @@ namespace epics { namespace exampleCPP { namespace database {
 class ExampleHelloRPCRecord;
 typedef std::tr1::shared_ptr<ExampleHelloRPCRecord> ExampleHelloRPCRecordPtr;
 
-class ExampleHelloRPCRecordService;
-typedef std::tr1::shared_ptr<ExampleHelloRPCRecordService> ExampleHelloRPCRecordServicePtr;
-
-class epicsShareClass ExampleHelloRPCRecordService:
+/**
+ * @brief A PVRecord that implements a hello service accessed via a channelRPC request.
+ *
+ */
+class epicsShareClass ExampleHelloRPCRecord :
+    public epics::pvDatabase::PVRecord,
     public virtual epics::pvAccess::RPCService
 {
 public:
-    POINTER_DEFINITIONS(ExampleHelloRPCRecordService);
+    POINTER_DEFINITIONS(ExampleHelloRPCRecord);
+    /**
+     * @brief Create an instance of ExampleHelloRecord.
+     *
+     * @param recordName The name of the record.
+     * @return The new instance.
+     */
+    static  ExampleHelloRPCRecordPtr create(std::string const & recordName);
 
-    static ExampleHelloRPCRecordServicePtr create(ExampleHelloRPCRecordPtr const & pvRecord)
-    {
-        return ExampleHelloRPCRecordServicePtr(new ExampleHelloRPCRecordService(pvRecord));
-    } 
-    ~ExampleHelloRPCRecordService() {};
-
+     /**
+      * @brief Get the ExampleRPC service.
+      */
+    virtual epics::pvAccess::Service::shared_pointer getService(
+        epics::pvData::PVStructurePtr const & pvRequest);
+    /**
+      * @brief Process a request from the client
+      *
+      * @param args The request from the client
+      * @return The result.
+      */
     epics::pvData::PVStructurePtr request(
         epics::pvData::PVStructurePtr const & args
     );
-private:
-    ExampleHelloRPCRecordService(ExampleHelloRPCRecordPtr const & pvRecord)
-    : pvRecord(pvRecord) {}
-
-    ExampleHelloRPCRecordPtr pvRecord;
-};
-
-class epicsShareClass ExampleHelloRPCRecord :
-    public epics::pvDatabase::PVRecord
-{
-public:
-    POINTER_DEFINITIONS(ExampleHelloRPCRecord);
-    static  ExampleHelloRPCRecordPtr create(std::string const & recordName);
     virtual ~ExampleHelloRPCRecord() {}
-    virtual void destroy() {PVRecord::destroy();}
-    virtual bool init();
-    virtual void process() {PVRecord::process();}
-    virtual epics::pvAccess::Service::shared_pointer getService(
-        epics::pvData::PVStructurePtr const & pvRequest);
-    void put(epics::pvData::PVStringPtr const & pvFrom);
+    virtual bool init() {return false;}
 private :
     ExampleHelloRPCRecord(
         std::string const & recordName,
-        epics::pvData::PVStructurePtr const & pvResult);
+        epics::pvData::PVStructurePtr const & pvTop);
 
-    epics::pvData::PVStructurePtr pvResult;
+    epics::pvData::PVStructurePtr put(
+        epics::pvData::PVStringPtr const & pvFrom);
+
+    epics::pvData::PVStructurePtr pvTop;
     epics::pvAccess::Service::shared_pointer service;
-    friend class ExampleHelloRPCRecordService;
 };
 
 }}}
