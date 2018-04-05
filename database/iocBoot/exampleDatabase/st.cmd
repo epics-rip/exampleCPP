eb154e87cf961234a12993c40da73d21e763eeb7
mrkraimer <mrkraimer@comcast.net>
2018-04-05 06:33:48 -0400

    add record types for DBF_CHAR, DBF_UCHAR, DBF_SHORT, DBF_USHORT, and DBF_ULONG

--- database/iocBoot/exampleDatabase/st.cmd
+++ database/iocBoot/exampleDatabase/st.cmd
@@ -10,7 +10,6 @@ exampleDatabase_registerRecordDeviceDriver(pdbbase)
 dbLoadRecords("db/dbStringArray.db","name=DBRstringArray01")
 dbLoadRecords("db/dbEnum.db","name=DBRenum01")
 dbLoadRecords("db/dbCounter.db","name=DBRcounter01")
-dbLoadRecords("db/dbSimpleBusy.db","name=DBRbusy")
 dbLoadRecords("db/dbArray.db","name=DBRdoubleArray,type=DOUBLE")
 dbLoadRecords("db/test.db")
 
